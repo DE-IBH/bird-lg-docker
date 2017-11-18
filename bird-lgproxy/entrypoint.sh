7a0f5cc0d444b1f3a17010997a1080b4de1b44ca
Thomas Liske <thomas@fiasko-nw.net>
2017-11-18 17:00:09 +0100

    Chown /dev/stdout to make logging to stdout work.
    See also:
    - https://github.com/moby/moby/issues/6880
    - https://github.com/moby/moby/pull/16468

--- bird-lgproxy/entrypoint.sh
+++ bird-lgproxy/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-exec gunicorn -u "${LGUID}" -g "${LGGID}" -b 0.0.0.0:8000 $@
+# allow bird-lg to log to STDOUT
+chown "${LGUID}:${LGGID}" /dev/stdout
 
+exec gunicorn -u "${LGUID}" -g "${LGGID}" -b 0.0.0.0:8000 $@
